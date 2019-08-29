7077abb27a20f6137013835e5f78df091639ce5d
laols574 <31547222+laols574@users.noreply.github.com>
2019-08-29 11:47:22 -0700

    Update net_server.py

--- net_server.py
+++ net_server.py
@@ -1,25 +1,29 @@
 '''
 This is the server side
-
+This code creates a socket, decides which port it will be attached to and starts listening to it. Once it receives a message,
+it turns it into a string and then sends it back to the client. 
 '''
 
 import socket
 
+#initialize port and create socket
 HOST = ""
 PORT = 3429
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#binds to port
 s.bind((HOST, PORT))
 
 s.listen(1)
 
 conn, addr = s.accept()
 
+#this while loop allows the data to be recieved
 while(1):
 	data = conn.recv(1024).decode()
 	if not data: break
-	conn.sendall(data)
 	print(data)
+	data += "This message has been received"
+	conn.sendall(data)
 
 conn.close() 
