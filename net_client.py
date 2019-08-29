81d2eb3f79cec5dbb0b1ae0c0740b505a59bbd3e
laols574 <31547222+laols574@users.noreply.github.com>
2019-08-29 11:43:47 -0700

    Update net_client.py
    added comments and cleaned up

--- net_client.py
+++ net_client.py
@@ -1,5 +1,8 @@
 """
 This is the client
+This code thus sends a message to the server. It does this through creating a socket, connecting it to the server, 
+it then creates a message (the string "this is a message") and sends it to the server. It then executes recv to receive
+any messages from the server. Finally, it must close the socket so it can be potentially reused. 
 """
 
 import socket
@@ -7,10 +10,16 @@ import socket
 
 HOST = ""
 PORT = 3429
+
+#socket.AFNET and socket.SOCK_STREAM are kernel constants 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+s.connect((HOST, PORT)) #connects to the port
+
+#create and send message
 msg = "This is the message"
 s.sendall(msg.encode())
+
+#receive data and close
 data = s.recv(1024).decode()
 s.close()
 
