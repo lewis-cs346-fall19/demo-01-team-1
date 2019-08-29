f105f4276381f5df5c8646ce19ababd3b3b47149
Lauren Olson <laols574@dachshund.cs.arizona.edu>
2019-08-29 10:25:57 -0700

    this is the server

--- net_server.py
+++ net_server.py
@@ -1,3 +1,33 @@
-"""
+'''
 This is the server side
-"""
+
+'''
+
+
+import socket
+
+
+
+def main():
+	host = ""
+	port = 3429
+	
+	s = socket.socket(AF_INET, SOCK_STREAM)
+
+	s.bind((host, port))
+
+	s.listen(1)
+
+	conn, addr = s.accept()
+
+	while(1):
+		data = conn.recv(1024)
+		if not data: break
+		conn.sendall(data)
+
+	conn.close() 
+	
+
+
+if __name__ == "__main__":
+	main()
