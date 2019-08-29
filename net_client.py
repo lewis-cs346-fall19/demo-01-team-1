ea9c97b913e6098460080944c875a9ffef4ff30e
Lauren Olson <laols574@dachshund.cs.arizona.edu>
2019-08-29 10:34:24 -0700

    this is the client

--- net_client.py
+++ net_client.py
@@ -1,3 +1,18 @@
 """
 This is the client
 """
+
+import socket
+
+
+HOST = "this.host"
+PORT = 3429
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+msg = "This is the message"
+s.sendall(msg.encode())
+data = s.recv(1024).decode()
+s.close()
+
+
+
