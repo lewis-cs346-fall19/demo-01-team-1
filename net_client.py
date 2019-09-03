e1a315d01af7bdfb23e5cad4e3dca0762566ae67
Jabin Matthew Tome <jabintome@cs.arizona.edu>
2019-09-03 10:18:09 -0700

    This is a test push message.

--- net_client.py
+++ net_client.py
@@ -3,6 +3,7 @@ This is the client
 This code thus sends a message to the server. It does this through creating a socket, connecting it to the server, 
 it then creates a message (the string "this is a message") and sends it to the server. It then executes recv to receive
 any messages from the server. Finally, it must close the socket so it can be potentially reused. 
+This is an extra test comment.
 """
 
 import socket
